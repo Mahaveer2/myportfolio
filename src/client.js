d2182626875c4e3a05791015f502743d9f199f62
Partab Rai <amrani.partab@gmail.com>
2023-01-10 13:06:54 +0500

    initial commit

--- src/client.js
+++ src/client.js
@@ -2,12 +2,14 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: 'hmm5oomy',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token:'skLahob9TPzIqzGgr3ydbRnBwNWMppPyWkVc9FFS0ZCxapyHuAOGsutompk7cMc3Ae7jzOrFtSiuHv8f7PaEnF2XptihA2QDP1S5K4Jv6oquXA8MuqnQfGOi9Cp4doArJjP6Bv4tm2Z6MhgioXzzs3FH8QqmHeEzXgql1xU8L9SAAiIIGnmU',
 });
+
+
 
 const builder = imageUrlBuilder(client);
 
